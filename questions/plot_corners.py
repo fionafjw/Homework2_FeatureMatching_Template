e3f3da906b6961d501031168cc0f4a175519e196
woody-hulse <wood_hulse@brown.edu>
2024-08-26 15:29:37 -0400

    Add parameters

--- questions/plot_corners.py
+++ questions/plot_corners.py
@@ -20,43 +20,44 @@ subpixel position of corners ([1]_, [2]_).
 
 from matplotlib import pyplot as plt
 
+import argparse
+
 from skimage import io, color, transform
 from skimage.feature import corner_harris, peak_local_max
 
-# load in different images to see where the Harris Corner Detector finds corners
+def main():
+    parser = argparse.ArgumentParser(description='Harris corner detector')
+    parser.add_argument('example', choices=['RISHLibrary', 'LaddObservatory', 'Chase'],
+                        help='Select the image pair for corner detection')
+    args = parser.parse_args()
 
-##############
-# TODO: Modify this to be one of:
-# - 'LaddObservatory'
-# - 'RISHLibrary'
-# - 'Chase'
-EXAMPLE = 'RISHLibrary'
-##############
+    image1 = transform.rescale(color.rgb2gray(io.imread(f'images/{args.example}1.jpg')),0.25)
+    image2 = transform.rescale(color.rgb2gray(io.imread(f'images/{args.example}2.jpg')),0.25)
 
-image1 = transform.rescale(color.rgb2gray(io.imread(f'images/{EXAMPLE}1.jpg')),0.25)
-image2 = transform.rescale(color.rgb2gray(io.imread(f'images/{EXAMPLE}2.jpg')),0.25)
+    harris_response1 = corner_harris(image1)
+    harris_response2 = corner_harris(image2)
 
-harris_response1 = corner_harris(image1)
-harris_response2 = corner_harris(image2)
+    ##############
+    # TODO: Feel free to play with these parameters to investigate their effects
+    min_distance = 5
+    threshold_rel = 0.05
+    ##############
 
-##############
-# TODO: Feel free to play with these parameters to investigate their effects
-min_distance = 5
-threshold_rel = 0.05
-##############
+    coords1 = peak_local_max( harris_response1, min_distance=min_distance, threshold_rel=threshold_rel )
+    coords2 = peak_local_max( harris_response2, min_distance=min_distance, threshold_rel=threshold_rel )
 
-coords1 = peak_local_max( harris_response1, min_distance=min_distance, threshold_rel=threshold_rel )
-coords2 = peak_local_max( harris_response2, min_distance=min_distance, threshold_rel=threshold_rel )
+    fig, ax = plt.subplots(1, 2, figsize=(6, 6))
 
-fig, ax = plt.subplots(1, 2, figsize=(6, 6))
+    ax[0].imshow(image1, cmap=plt.cm.gray)
+    ax[0].plot(coords1[:, 1], coords1[:, 0], '+r', markersize=15)
+    ax[0].axis((0, image1.shape[1], image1.shape[0], 0))
+    ax[0].axis('off')
 
-ax[0].imshow(image1, cmap=plt.cm.gray)
-ax[0].plot(coords1[:, 1], coords1[:, 0], '+r', markersize=15)
-ax[0].axis((0, image1.shape[1], image1.shape[0], 0))
-ax[0].axis('off')
+    ax[1].imshow(image2, cmap=plt.cm.gray)
+    ax[1].plot(coords2[:, 1], coords2[:, 0], '+r', markersize=15)
+    ax[1].axis('off')
 
-ax[1].imshow(image2, cmap=plt.cm.gray)
-ax[1].plot(coords2[:, 1], coords2[:, 0], '+r', markersize=15)
-ax[1].axis('off')
+    plt.show()
 
-plt.show()
+if __name__ == '__main__':
+    main()
